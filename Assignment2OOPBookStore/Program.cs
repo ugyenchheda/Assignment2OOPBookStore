67f11fa67cbe5672809a3f1bf960262f603e4903
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-02-19 00:33:38 +0200

    Update Program.cs

--- Assignment2OOPBookStore/Program.cs
+++ Assignment2OOPBookStore/Program.cs
@@ -56,22 +56,6 @@ namespace Assignment2OOPBookStore
 
         List<Book> books = new List<Book>()
         {
-            new Book("Little Women", 19.85),
-            new Book("The Trial", 37.05),
-            new Book("The Grapes of Wrath", 29.95),
-            new Book("Great Expectations", 22.25),
-            new Book("Dead Souls", 17.45),
-            new Book("For Whom the Bells Tolls", 31.25),
-            new Book("Vanity Fair", 22.95),
-            new Book("The Old Man and the Sea", 29.65),
-            new Book("The Tin Drum", 16.65),
-            new Book("Les Miserables", 24.40),
-            new Book("Wall Street Coyotes, part 1", 23.80),
-            new Book("Wall Street Coyotes, part 2", 24.70),
-            new Book("Wall Street Coyotes, part 3", 25.60),
-            new Book("Fire and Fury", 30.00),
-            new Book("The Woman in the Window", 21.20),
-            new Book("A Higher Loyalty: Truth, Lies, and Leadership", 19.45),
             new Book("The Miserable Ones", 23.85),
             new Book("East of Eden", 31.10),
             new Book("Anna Karenina", 31.45),
@@ -100,7 +84,23 @@ namespace Assignment2OOPBookStore
             new Book("War and Peace", 30.55),
             new Book("The five love languages", 13.70),
             new Book("The Brothers Karamazov", 33.35),
-            new Book("Wuthering Heights", 27.75)
+            new Book("Wuthering Heights", 27.75),
+            new Book("Little Women", 19.85),
+            new Book("The Trial", 37.05),
+            new Book("The Grapes of Wrath", 29.95),
+            new Book("Great Expectations", 22.25),
+            new Book("Dead Souls", 17.45),
+            new Book("For Whom the Bells Tolls", 31.25),
+            new Book("Vanity Fair", 22.95),
+            new Book("The Old Man and the Sea", 29.65),
+            new Book("The Tin Drum", 16.65),
+            new Book("Les Miserables", 24.40),
+            new Book("Wall Street Coyotes, part 1", 23.80),
+            new Book("Wall Street Coyotes, part 2", 24.70),
+            new Book("Wall Street Coyotes, part 3", 25.60),
+            new Book("Fire and Fury", 30.00),
+            new Book("The Woman in the Window", 21.20),
+            new Book("A Higher Loyalty: Truth, Lies, and Leadership", 19.45)
         };
 
         public List<Book> FindBook(string keyword)
@@ -148,8 +148,5 @@ namespace Assignment2OOPBookStore
             }
             return totalPrice;
         }
-        
     }
-
-
 }
